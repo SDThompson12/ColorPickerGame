816d6503e520ee36365424b3f6626382bf918357
root <sean.d.thompson12@gmail.com>
2020-08-01 03:21:01 +0000

    Updated Color Picker game to run as a server-side application

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+// '__dirname' - Just references to the current directory path Ex) .../workspace/seanjr-personaltests/MyPersonalSite
+
+//Requiring Routes
+var colorPickerRoute = require('./colorPicker.js');
+
+
+//Imports Routes
+app.use("/", colorPickerRoute);
+
+app.listen(3420, function()
+{
+    console.log("Starting My Color Picking game...");
+});
